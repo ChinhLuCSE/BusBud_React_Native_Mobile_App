d07979c320bc4b8deb319263e106241ec77635fa
Vu Dang Khoa <87051112+khoavukhoa3004@users.noreply.github.com>
2023-05-25 20:21:12 +0700

    modified:   App.js 	modified:   package-lock.json 	modified:   package.json

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import { StatusBar } from "expo-status-bar";
 import { KeyboardAvoidingView, Platform } from "react-native";
 import { Provider } from "react-redux";
 import store from "./store";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import * as React from 'react';
 
 import HomeScreen from "./screens/HomeScreen";
 import MapScreen from "./screens/MapScreen";
@@ -29,7 +31,17 @@ import Payments from "./screens/Payments";
 import AccountSetting from "./screens/AccountSetting";
 import MenuOverlay from "./screens/MenuOverlay";
 
+const checkFirstLaunch = async () => {
+  const isFirstLaunch = await AsyncStorage.getItem('alreadyLaunched');
+  if(isFirstLaunch == null) {
+    AsyncStorage.setItem('alreadyLaunched', 'true');
+    return true;
+  }
+  return false;
+};
+
 export default function App() {
+  const [isFirstLaunch, setIsFirstLaunch] = React.useState(null);
   const Stack = createNativeStackNavigator();
   const [fontsLoaded, error] = useFonts({
     Roboto_medium: require("./assets/fonts/Roboto_medium.ttf"),
@@ -43,10 +55,18 @@ export default function App() {
     // "SF Pro Display_medium": require("./assets/fonts/SF_Pro_Display_medium.ttf"),
   });
 
+  React.useEffect(() => {
+    checkFirstLaunch().then(result => setIsFirstLaunch(result));
+  }, []);
+
   if (!fontsLoaded && !error) {
     return null;
   }
-  return (
+
+  if (isFirstLaunch === null) {
+    return null;
+  } else if (isFirstLaunch === true) {
+    return (
     <Provider store={store}>
       <NavigationContainer>
         <SafeAreaProvider>
@@ -154,4 +174,84 @@ export default function App() {
       </NavigationContainer>
     </Provider>
   );
+  } else {
+    return(
+      <Provider store={store}>
+      <NavigationContainer>
+        <SafeAreaProvider>
+          <KeyboardAvoidingView
+            behavior={Platform.OS === "ios" ? "padding" : "height"}
+            style={{ flex: 1 }}
+            keyboardVerticalOffset={Platform.OS === "ios" ? -64 : 0}
+          >
+            <Stack.Navigator>
+              <Stack.Screen
+                name="HomeScreen"
+                component={HomeScreen}
+                options={{
+                  headerShown: false,
+                }}
+              />
+              <Stack.Screen
+                name="MenuOverlay"
+                component={MenuOverlay}
+                options={{
+                  headerShown: false,
+                }}
+              />              
+              <Stack.Screen
+                name="QR1"
+                component={QR1}
+                options={{
+                  headerShown: false,
+                }}
+              />
+               <Stack.Screen
+                name="QR2"
+                component={QR2}
+                options={{
+                  headerShown: false,
+                }}
+              />
+               <Stack.Screen
+                name="QR3"
+                component={QR3}
+                options={{
+                  headerShown: false,
+                }}
+              />
+              <Stack.Screen
+                name="Setting"
+                component={Setting}
+                options={{ headerShown: false }}
+              />
+              <Stack.Screen
+                name="Notifications"
+                component={Notifications}
+                options={{ headerShown: false }}
+              />
+              <Stack.Screen
+                name="Payments"
+                component={Payments}
+                options={{ headerShown: false }}
+              />
+              <Stack.Screen
+                name="AccountSetting"
+                component={AccountSetting}
+                options={{ headerShown: false }}
+              />
+              <Stack.Screen
+                name="MapScreen"
+                component={MapScreen}
+                options={{
+                  headerShown: false,
+                }}
+              />
+            </Stack.Navigator>
+          </KeyboardAvoidingView>
+        </SafeAreaProvider>
+      </NavigationContainer>
+    </Provider>
+    );
+  }
 }
