b42f43a8634bf1731e3726047e0cd76bff557b28
ChinhLuCSE <100012219+ChinhLuCSE@users.noreply.github.com>
2023-05-27 18:21:47 +0700

    modified:   components/DoItLaterOverlay.js 	modified:   components/RideOptionsCard.js

--- components/RideOptionsCard.js
+++ components/RideOptionsCard.js
@@ -1,83 +1,93 @@
-import { StyleSheet, Text, View, SafeAreaView, TouchableOpacity, FlatList, Image } from 'react-native'
-import React, { useState } from 'react'
-import tw from 'twrnc'
-import { Icon } from '@rneui/base'
-import { useNavigation } from '@react-navigation/native'
-import { useSelector } from 'react-redux'
-import { selectTravelTimeInformation } from '../slices/navSlice'
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  TouchableOpacity,
+  FlatList,
+  Image,
+} from "react-native";
+import React, { useState } from "react";
+import tw from "twrnc";
+import { Icon } from "@rneui/base";
+import { useNavigation } from "@react-navigation/native";
+import { useSelector } from "react-redux";
+import { selectTravelTimeInformation } from "../slices/navSlice";
 
 const data = [
-    {
-        id: "50",
-        title: "Bus 50", // vehicle type
-        multiplier: 1, //cost
-        image: require("../assets/bus.png"),
-    },
-    // {
-    //     id: "8",
-    //     title: "Bus 8",
-    //     multiplier: 1,
-    //     image: "",
-    // },
-    // {
-    //     id: "150",
-    //     title: "Xe Bus",
-    //     multiplier: 1,
-    //     image: "",
-    // },
-]
+  {
+    id: "50",
+    title: "Bus 50", // vehicle type
+    multiplier: 1, //cost
+    image: require("../assets/bus.png"),
+  },
+  // {
+  //     id: "8",
+  //     title: "Bus 8",
+  //     multiplier: 1,
+  //     image: "",
+  // },
+  // {
+  //     id: "150",
+  //     title: "Xe Bus",
+  //     multiplier: 1,
+  //     image: "",
+  // },
+];
 
 const SURGE_CHARGE_RATE = 1.5;
 
 const RideOptionsCard = () => {
-    const navigation = useNavigation();
-    const [selected, setSelected] = useState(null);
-    const travelTimeInformation = useSelector(selectTravelTimeInformation);
+  const navigation = useNavigation();
+  const [selected, setSelected] = useState(null);
+  const travelTimeInformation = useSelector(selectTravelTimeInformation);
 
   return (
     <SafeAreaView style={tw`bg-white flex-grow`}>
-        <View>
-            <TouchableOpacity 
-                onPress={() => navigation.navigate('NavigateCard')}
-                style={tw`absolute top-3 left-5 z-50 p-3 rounded-full`}
-            >
-                <Icon 
-                    name="chevron-left"
-                    type='fontawesome'
-                />
-            </TouchableOpacity>
-            <Text style={tw`text-center py-5 text-xl`}>Get a Bus{travelTimeInformation?.distance?.text}</Text>
-        </View>
+      <View>
+        <TouchableOpacity
+          onPress={() => navigation.navigate("NavigateCard")}
+          style={tw`absolute top-3 left-5 z-50 p-3 rounded-full`}
+        >
+          <Icon name="chevron-left" type="fontawesome" />
+        </TouchableOpacity>
+        <Text style={tw`text-center py-5 text-xl`}>
+          Get a Bus{travelTimeInformation?.distance?.text}
+        </Text>
+      </View>
 
-        <FlatList 
-            data={data}
-            keyExtractor={(item) => item.id}
-            renderItem={({item: {id, title, multiplier, image}, item}) => (
-                <TouchableOpacity 
-                    onPress={() => setSelected(item)}
-                    style={tw`flex-row justify-between items-center px-10 ${id === selected?.id && "bg-gray-200"}`}>
-                    <Image 
-                        style={{
-                            width: 50,
-                            height: 50,
-                            resizeMode: "contain"
-                        }}
-                        source={image}
-                    />
-                    <View style={tw`-ml-6`}>
-                        <Text style={tw`text-xl font-semibold`}>{title}</Text>
-                        <Text>{travelTimeInformation?.distance.text}1 hours 30 mins </Text>
-                    </View>
-                    <Text style={tw`text-xl `} >7.000đ</Text>
-                </TouchableOpacity>
-            )}
-        />
-
-        
+      <FlatList
+        data={data}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item: { id, title, multiplier, image }, item }) => (
+          <TouchableOpacity
+            onPress={() => setSelected(item)}
+            style={tw`flex-row justify-between items-center px-10 ${
+              id === selected?.id && "bg-gray-200"
+            }`}
+          >
+            <Image
+              style={{
+                width: 50,
+                height: 50,
+                resizeMode: "contain",
+              }}
+              source={image}
+            />
+            <View style={tw`-ml-6`}>
+              <Text style={tw`text-xl font-semibold`}>{title}</Text>
+              <Text>
+                {travelTimeInformation?.distance.text}1 hours 30 mins{" "}
+              </Text>
+            </View>
+            <Text style={tw`text-xl `}>7.000đ</Text>
+          </TouchableOpacity>
+        )}
+      />
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default RideOptionsCard
+export default RideOptionsCard;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
